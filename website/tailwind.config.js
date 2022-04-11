826940e2b79688fc13287928a0ea0390b0a652b7
Mackenzie Choy <mackenziechoy@gmail.com>
2022-04-10 22:11:45 -0400

    Add basic theming options

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          primary: 'var(--color-text-primary)',
+          secondary: 'var(--color-text-secondary)',
+        },
+      },
+      backgroundColor: {
+        skin: {
+          'fill-primary': 'var(--color-fill-primary)',
+          'fill-secondary': 'var(--color-fill-secondary)',
+          'button-primary': 'var(--color-button-primary)',
+          'button-primary-hover': 'var(--color-button-primary-hover)',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
